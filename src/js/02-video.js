3fdc5b22af653ecd452d373b58078c19a8a2a306
SirotovAlexander <alexander.sirotov@gmail.com>
2023-04-11 01:52:32 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,9 +3,13 @@ import throttle from 'lodash.throttle';
 
 const inframe = document.querySelector('#vimeo-player');
 const player = new Vimeo(inframe);
-const localStorage = 'videoplayer-current-time';
-player.on('play', onPlay);
+const localeKey = 'videoplayer-current-time';
 
-function onPlay(data) {
-  console.log(data.seconds);
-}
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem(localeKey, JSON.stringify(data.seconds));
+  }, 1000)
+);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
