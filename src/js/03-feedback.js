9a75cb9c219a88e9fefc8f46b308c71ee5054d1c
SirotovAlexander <alexander.sirotov@gmail.com>
2023-04-11 03:33:24 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,9 +6,8 @@ const text = document.querySelector('textarea');
 
 const STORAGE_KEY = 'feedback-form-state';
 const STORAGE_OBJECT = {};
-// console.dir(form);
-// console.dir(inPut);
-// console.dir(text);
+
+checkSave();
 
 form.addEventListener('submit', onSubmit);
 inPut.addEventListener('input', throttle(onInputMail, 500));
@@ -17,16 +16,26 @@ text.addEventListener('input', throttle(onInputText, 500));
 function onSubmit(event) {
   event.preventDefault();
   event.currentTarget.reset();
+  localStorage.clear();
 }
 
 function onInputMail(event) {
-  // console.log(event.target.value);
   STORAGE_OBJECT.email = event.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(STORAGE_OBJECT));
 }
 
 function onInputText(event) {
-  // console.log(event.target.value);
   STORAGE_OBJECT.message = event.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(STORAGE_OBJECT));
+}
+
+function checkSave() {
+  const save = localStorage.getItem(STORAGE_KEY);
+
+  if (save) {
+    const parseFromSave = JSON.parse(save);
+
+    inPut.value = parseFromSave.email;
+    text.value = parseFromSave.message;
+  }
 }
